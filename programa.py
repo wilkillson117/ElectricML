7afe560ab38c93436302acd5785174fbdff05cfd
wilkillson117 <wilsonvillamar090899@gmail.com>
2023-08-21 19:09:10 -0500

    first xgboost model deploy

--- programa.py
+++ programa.py
@@ -1,17 +1,36 @@
 import streamlit as st
-import time
+import datetime
+import pandas as pd
+import numpy as np
 
-'Starting a long computation...'
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from numpy import sqrt
+modelo = XGBRegressor()
+modelo.load_model('XGBoost1.json')
 
-# Add a placeholder
-time.sleep(4)
-latest_iteration = st.empty()
-bar = st.progress(0)
+# Crear un rango de fechas con una frecuencia de 15 segundos para el 19 de mayo de 2023
+fecha_inicio = st.date_input("Ingrese fecha de inicio", datetime.date(2023, 5, 19))
+fecha_fin = st.date_input("Ingrese fecha fin", datetime.date(2023, 5, 20))
 
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+st.write(str(fecha_inicio) + " hasta " + str(fecha_fin))
 
-'...and now we\'re done!'
+indice_fecha = pd.date_range(start=fecha_inicio, end=fecha_fin, freq='15S')
+
+# Crear un DataFrame usando este rango de fechas como índice y con una columna ficticia
+dataset = pd.DataFrame(index=indice_fecha)
+dataset['Hora'] = dataset.index.hour
+dataset['Día de Semana'] = dataset.index.dayofweek
+dataset['Mes'] = dataset.index.month
+dataset['Día del Año'] = dataset.index.dayofyear
+dataset['Día del Mes'] = dataset.index.day
+dataset['Semana del Año'] = dataset.index.isocalendar().week.astype(int)
+dataset['Es Fin de Semana'] = (dataset['Día de Semana'] >= 5).astype(int)
+dataset.index.tz_localize(None)
+
+dataset
+
+dataset['Predicción'] = modelo.predict(dataset)
+
+# Crear el gráfico
+st.line_chart(dataset, x=None, y='Predicción', width=0, height=0, use_container_width=True)
