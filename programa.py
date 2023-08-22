c9d1730b3b6a94b7f71e8bf292e8ff5b0a93ed77
wilkillson117 <wilsonvillamar090899@gmail.com>
2023-08-22 08:55:16 -0500

    second xgboost

--- programa.py
+++ programa.py
@@ -8,6 +8,11 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 from numpy import sqrt
 modelo = XGBRegressor()
 modelo.load_model('XGBoost1.json')
+modelo1 = XGBRegressor()
+modelo1.load_model('WVXGBoost.json')
+
+st.title('Predicción de consumo de energía')
+st.divider()
 
 # Crear un rango de fechas con una frecuencia de 15 segundos para el 19 de mayo de 2023
 fecha_inicio = st.date_input("Ingrese fecha de inicio", datetime.date(2023, 5, 19))
@@ -28,9 +33,18 @@ dataset['Semana del Año'] = dataset.index.isocalendar().week.astype(int)
 dataset['Es Fin de Semana'] = (dataset['Día de Semana'] >= 5).astype(int)
 dataset.index.tz_localize(None)
 
-dataset
+if st.checkbox('Mostrar tabla'):
+    st.write(dataset)
 
-dataset['Predicción'] = modelo.predict(dataset)
+caracteristicas = ['Hora', 'Día de Semana', 'Mes', 'Día del Año', 'Día del Mes', 'Semana del Año', 'Es Fin de Semana']
+
+dataset['Predicción'] = modelo.predict(dataset[caracteristicas])
+dataset['Predicción1'] = modelo1.predict(dataset[caracteristicas])
+
+predicciones = dataset[['Predicción','Predicción1']]
 
 # Crear el gráfico
-st.line_chart(dataset, x=None, y='Predicción', width=0, height=0, use_container_width=True)
+st.line_chart(dataset, x=None, y=['Predicción','Predicción1'], width=0, height=0, use_container_width=True)
+st.divider()
+if st.checkbox('Mostrar Predicciones'):
+    st.write(predicciones)
